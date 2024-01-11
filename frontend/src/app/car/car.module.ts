47882e0d2832238de64e24468b5906e22bd5856a
Silas <silas.zahner@gmail.com>
2024-01-11 19:35:26 +0100

    added fitlers for car rentals

--- frontend/src/app/car/car.module.ts
+++ frontend/src/app/car/car.module.ts
@@ -11,7 +11,9 @@ import { SliderModule } from 'primeng/slider';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { GalleriaModule } from 'primeng/galleria';
 import { ButtonModule } from 'primeng/button';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -28,8 +30,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     InputNumberModule,
     GalleriaModule,
     ButtonModule,
-    ReactiveFormsModule,
-    
+    FormsModule,
+    InputTextModule,
+    BrowserAnimationsModule,
   ]
 })
 export class CarModule { }
--- frontend/src/app/car/components/car-rental-filter/car-rental-filter.component.ts
+++ frontend/src/app/car/components/car-rental-filter/car-rental-filter.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './car-rental-filter.component.scss'
 })
 export class CarRentalFilterComponent {
-
+  selectedBrand_1: any;
+  rangeValues: number[] = [100, 400];
+  selectedColors: any[] = [];
+  selectedSizes1: any[] = [];
 }
