865ae3ac2af887aa64a37999b4052f5d70c08ae8
Silas <silas.zahner@gmail.com>
2024-01-18 16:36:31 +0100

    cross origin

--- backend/car_rental_service/src/main/kotlin/org/rental/car_rental/controller/CustomerController.kt
+++ backend/car_rental_service/src/main/kotlin/org/rental/car_rental/controller/CustomerController.kt
@@ -7,31 +7,28 @@ import org.rental.car_rental.model.Customer
 import org.rental.car_rental.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/v1/customers")
 class CustomerController(private val customerService: CustomerService) {
     @GetMapping
+    @CrossOrigin
     fun getCustomers(): List<CustomerGetDto> = customerService.getAllCustomers()
 
     @GetMapping("/{id}")
+    @CrossOrigin
     fun getCustomer(@PathVariable id: Long): Customer = customerService.getCustomerById(id)
 
     @PostMapping
+    @CrossOrigin
     fun postCustomer(@Valid @RequestBody customerDto: CustomerCreateDto): ResponseEntity<Customer> {
         val newCustomer = customerService.createCustomer(customerDto)
         return ResponseEntity(newCustomer, HttpStatus.CREATED)
     }
 
     @PutMapping("/{id}")
+    @CrossOrigin
     fun putCustomer(
         @PathVariable id: Long,
         @Valid @RequestBody customerDto: CustomerCreateDto,
@@ -41,6 +38,7 @@ class CustomerController(private val customerService: CustomerService) {
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin
     fun deleteCustomer(@PathVariable id: Long): ResponseEntity<Map<String, String>> {
         val deletedCustomer = customerService.deleteCustomer(id)
         return ResponseEntity(
