1d2538a03099512724801b75f0335022c4dc1d75
Rocco Ciccone <rocco.ciccone@proton.me>
2024-01-19 03:20:38 +0100

    feat: added empty service tests

---
+++ backend/car_rental_service/src/test/kotlin/org/rental/car_rental/service/CarServiceImplTest.kt
@@ -0,0 +1,28 @@
+package org.rental.car_rental.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CarServiceImplTest {
+
+    @Test
+    fun getAllCars() {
+    }
+
+    @Test
+    fun getCarsById() {
+    }
+
+    @Test
+    fun createCar() {
+    }
+
+    @Test
+    fun updateCar() {
+    }
+
+    @Test
+    fun deleteCar() {
+    }
+}
---
+++ backend/car_rental_service/src/test/kotlin/org/rental/car_rental/service/CustomerServiceImplTest.kt
@@ -0,0 +1,28 @@
+package org.rental.car_rental.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CustomerServiceImplTest {
+
+    @Test
+    fun getAllCustomers() {
+    }
+
+    @Test
+    fun getCustomerById() {
+    }
+
+    @Test
+    fun createCustomer() {
+    }
+
+    @Test
+    fun updateCustomer() {
+    }
+
+    @Test
+    fun deleteCustomer() {
+    }
+}
